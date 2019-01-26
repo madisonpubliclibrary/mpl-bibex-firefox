8d4252bae8b41cdc4f3be12d90b30c3a93743c7c
Lucas Schneider <lrschneider94@gmail.com>
2019-01-26 16:52:57 -0600

    progress

--- background/main.js
+++ background/main.js
@@ -49,7 +49,7 @@
     }
   });
 }
-  
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.key) {
     case "updateExtensionIcon":
@@ -64,3 +64,15 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
       break;
   }
 });
+
+//JSON Request w/out jquery
+/*var xmlhttp = new XMLHttpRequest();
+var url = "https://mpl-bibex.lrschneider.com/pstats/ver/?val=all&regex=true";
+
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText)
+  }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();*/
--- content/scripts/standardFormat.js
+++ content/scripts/standardFormat.js
@@ -1,37 +1,29 @@
-//var innerFrame = document.getElementsByTagName("iframe"),
-//  innerDoc;
-
-//if (innerFrame && innerFrame.length > 0) {
-//  innerFrame = innerFrame[0];
-//  innerDoc = innerFrame.contentDocument;
-console.log("BIBEX: "+window.location.toString())
-  if (/bibliovation\.com\/cgi-bin\/koha\/members\/memberentry\.pl/.test(window.location.toString())) {
-    /*** Control-space to save patron record ***/
-    document.addEventListener("keydown", function (e) {
-      if (e.keyCode === 32 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        e.preventDefault();
-        var updateAndSave = document.getElementById('updateAndSave'),
-          saveButton = document.getElementsByClassName('action'); // Wrapping elt.
-        if (updateAndSave) {
-          updateAndSave.click();
-        } else if (saveButton.length > 0) {
-          saveButton = saveButton[0].children[0];
-          if (saveButton) {
-            saveButton.click();
-          }
+if (/bibliovation\.com\/cgi-bin\/koha\/members\/memberentry\.pl/.test(window.location.toString())) {
+  // Control-space to save patron record
+  document.addEventListener("keydown", function (e) {
+    if (e.keyCode === 32 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      e.preventDefault();
+      var updateAndSave = document.getElementById('updateAndSave'),
+        saveButton = document.getElementsByClassName('action');
+      if (updateAndSave) {
+        updateAndSave.click();
+      } else if (saveButton.length > 0) {
+        saveButton = saveButton[0].children[0];
+        if (saveButton) {
+          saveButton.click();
         }
       }
-    }, false);
+    }
+  }, false);
 
-    /*** Escape to exit editing patron record ***/
-    document.addEventListener("keydown", function (e) {
-      if (e.keyCode === 27) {
-        e.preventDefault();
-        var cancelButton = document.getElementsByClassName('cancel');
-        if (cancelButton.length > 0) {
-          cancelButton[0].click();
-        }
+  // Escape to exit editing patron record
+  document.addEventListener("keydown", function (e) {
+    if (e.keyCode === 27) {
+      e.preventDefault();
+      var cancelButton = document.getElementsByClassName('cancel');
+      if (cancelButton.length > 0) {
+        cancelButton[0].click();
       }
-    }, false);
-  }
-//}
+    }
+  }, false);
+}
