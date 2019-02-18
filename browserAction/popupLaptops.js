d927197e1160a44bcd06bcf6b13331d6881c401b
Jose Ornelas <ornelalf001@gmail.com>
2019-02-17 23:09:02 -0600

    set Input Filters
    Additional restrictions to only allow numerical values in the barcode field.

--- browserAction/popupLaptops.js
+++ browserAction/popupLaptops.js
@@ -1,3 +1,5 @@
+var barcode = document.getElementById("anyNumber").value;
+
 var defaultMenu = document.getElementById('defaultMenu'),
   laptopMenu = document.getElementById('laptopMenu'),
   laptopFormOn = document.getElementById("laptopFormOn"),
@@ -17,8 +19,26 @@ function updateContent() {
   });
 }
 
+function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    textbox.addEventListener(event, function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      }
+    });
+  });
+}    
+
 updateContent();
 
 laptopFormOnSwitch.addEventListener('click', function() {
   browser.storage.sync.set({"laptopFormChecked": laptopFormOn.checked}).then(updateContent);
 });
+
+setInputFilter(document.getElementById("anyNumber"), function(value) {
+  return /^\d*$/.test(value); });
