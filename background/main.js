a9ecea013bb61fe45ebd9f3922040521562a7420
Lucas Schneider <lschneider4@wisc.edu>
2019-12-13 15:41:11 -0600

    v1.0 Bibliovation production release

--- background/main.js
+++ background/main.js
@@ -647,7 +647,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
       return new Promise((resolve, reject) => {
         if (request.hasOwnProperty('patronBarcode')) {
           browser.tabs.create({
-            "url": "https://lakscls-sandbox.bibliovation.com/cgi-bin/koha/members/member.pl?member=" +
+            "url": "https://scls.kohalibrary.com/cgi-bin/koha/members/member.pl?member=" +
                 request.patronBarcode,
             "active": false
           }).then(tab => {
@@ -669,7 +669,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
         }
       }).then(patronID => {
         return browser.tabs.create({
-          "url": "https://lakscls-sandbox.bibliovation.com/cgi-bin/koha/members/moremember.pl?borrowernumber=" +
+          "url": "https://scls.kohalibrary.com/cgi-bin/koha/members/moremember.pl?borrowernumber=" +
               patronID,
           "active": false
         }).then(tab => {
@@ -685,7 +685,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     case "getItemData":
       return new Promise((resolve, reject) => {
         browser.tabs.create({
-          "url": "https://lakscls-sandbox.bibliovation.com/app/search/" + request.itemBarcode,
+          "url": "https://scls.kohalibrary.com/app/search/" + request.itemBarcode,
           "active": true
         }).then(tab => {
           browser.tabs.executeScript(tab.id, {
@@ -703,7 +703,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
         return browser.storage.sync.get('getItemUse').then(usePref => {
           let getItemTitleCopiesHolds = new Promise((resolve, reject) => {
             browser.tabs.create({
-              "url": "https://lakscls-sandbox.bibliovation.com/app/staff/bib/" +
+              "url": "https://scls.kohalibrary.com/app/staff/bib/" +
                   bibNum + "/details?mbxItemBC=" + request.itemBarcode,
               "active": true
             }).then(tab => {
@@ -719,7 +719,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
           if (usePref.hasOwnProperty('getItemUse') && usePref.getItemUse) {
             let getItemUse = new Promise((resolve, reject) => {
               browser.tabs.create({
-                "url": "https://lakscls-sandbox.bibliovation.com/app/staff/bib/" +
+                "url": "https://scls.kohalibrary.com/app/staff/bib/" +
                     bibNum + "/items/circstatus?mbxItemBC=" + request.itemBarcode,
                 "active": true
               }).then(tab => {
@@ -734,7 +734,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
             let getItemPastUse = new Promise((resolve, reject) => {
               browser.tabs.create({
-                "url": "https://lakscls-sandbox.bibliovation.com/app/staff/bib/" +
+                "url": "https://scls.kohalibrary.com/app/staff/bib/" +
                     bibNum + "/items?mbxItemBC=" + request.itemBarcode,
                 "active": true
               }).then(tab => {
